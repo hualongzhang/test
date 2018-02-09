354982406c2e4946cd31737c0b5fb2ffc681dca5
hualongzhang <346180397@qq.com>
2018-02-09 17:18:00 +0800

    去掉多余注释
    去掉多余注释

--- demo/src/main/java/com/example/demo/util/DemoUtils.java
+++ demo/src/main/java/com/example/demo/util/DemoUtils.java
@@ -1,19 +1,6 @@
 package com.example.demo.util;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
 import java.util.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.config.ConfigFileApplicationListener;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.support.SpringFactoriesLoader;
 
 /**
  * Created by zhanghualong on 2017/11/27.
@@ -26,9 +13,6 @@ public class DemoUtils {
         System.out.println(linked_list.toString());
         linked_list.add("123");
         System.out.println();
-        
-        SpringFactoriesLoader a=null;
-        ConfigFileApplicationListener aa=null;
     }
 
 }
